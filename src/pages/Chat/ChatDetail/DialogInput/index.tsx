2a7fd51ea5d56965fe53febea2633a38412800c4
caifeng01 <caifeng01@appledeMacBook-Pro.local>
2022-06-06 16:59:36 +0800

    feat: 添加回车提交与ctrl回车换号

--- src/pages/Chat/ChatDetail/DialogInput/index.tsx
+++ src/pages/Chat/ChatDetail/DialogInput/index.tsx
@@ -1,13 +1,36 @@
 import {Input} from 'antd';
+import {useState} from 'react';
 import {Wrapper} from './ui';
 
 const DialogInput = () => {
+    const [input, setInput] = useState<string>('');
+
+    const sendMessage = () => {
+        setInput('');
+    };
+
+    const onPressEnter = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (!e.metaKey) {
+            sendMessage();
+            e.preventDefault();
+        }
+        else {
+            setInput(inp => `${inp}\n`);
+        }
+    };
+
     return (
         <Wrapper>
             <div className="resize"></div>
             <div className="content">
                 <div className="toolbar">工具栏 表情 图片</div>
-                <Input.TextArea />
+                <Input.TextArea
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
+                    autoComplete="on"
+                    autoFocus
+                    onPressEnter={onPressEnter}
+                />
             </div>
         </Wrapper>
     );
